dd025abe9660bbb97672ac4dfa07813faadff08f
galtashma <galtashma@gmail.com>
2014-11-14 01:42:40 +0200

    added live editor to post page

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 // create the module and name it jigglyApp
-var jigglyApp = angular.module('jigglyApp', ['ngRoute']);
+var jigglyApp = angular.module('jigglyApp', ['ngRoute', 'textAngular']);
 
 // configure our routes
 jigglyApp.config(function($routeProvider) {
@@ -21,5 +21,4 @@ jigglyApp.config(function($routeProvider) {
 			controller  : 'postController'
 		})
 });
-
 	
