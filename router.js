34313f0494d7045465a6c23f517c99ecf47128c9
galtashma <galtashma@gmail.com>
2014-11-13 22:36:04 +0200

    removed unused commented code

--- router.js
+++ router.js
@@ -12,25 +12,4 @@ jigglyApp.config(function($routeProvider) {
 		})
 });
 
-	// // create the controller and inject Angular's $scope
-	// jigglyApp.controller('mainController', function($scope) {
-	// 	// create a message to display in our view
-	// 	console.log('ASasAS');
-	// 	console.log($scope);
-
-	// 	//$scope.discussions = [];
-	// 	$scope.discussions = [
-	// 		{title :'woho', firstComment : 'I like Jigglipeace',	author: 'Eitan Bolbol Gadol'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-	// 	];
-	// });
-
 	
