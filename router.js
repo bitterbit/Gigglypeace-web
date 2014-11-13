a03e28857ff04a14773c12214c429a5e96fdb632
galtashma <galtashma@gmail.com>
2014-11-13 22:34:59 +0200

    split controllers to files

--- router.js
+++ router.js
@@ -1,36 +1,36 @@
-	// create the module and name it jigglyApp
-	var jigglyApp = angular.module('jigglyApp', ['ngRoute']);
+// create the module and name it jigglyApp
+var jigglyApp = angular.module('jigglyApp', ['ngRoute']);
 
-	// configure our routes
-	jigglyApp.config(function($routeProvider) {
-		console.log($routeProvider);
-		$routeProvider
-			// route for the home page
-			.when('/', {
-				templateUrl : 'templates/home.html',
-				controller  : 'mainController'
-			})
-	});
+// configure our routes
+jigglyApp.config(function($routeProvider) {
+	console.log($routeProvider);
+	$routeProvider
+		// route for the home page
+		.when('/', {
+			templateUrl : 'templates/home.html',
+			controller  : 'homeController'
+		})
+});
 
-	// create the controller and inject Angular's $scope
-	jigglyApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		console.log('ASasAS');
-		console.log($scope);
+	// // create the controller and inject Angular's $scope
+	// jigglyApp.controller('mainController', function($scope) {
+	// 	// create a message to display in our view
+	// 	console.log('ASasAS');
+	// 	console.log($scope);
 
-		//$scope.discussions = [];
-		$scope.discussions = [
-			{title :'woho', firstComment : 'I like Jigglipeace',	author: 'Eitan Bolbol Gadol'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-		];
-	});
+	// 	//$scope.discussions = [];
+	// 	$scope.discussions = [
+	// 		{title :'woho', firstComment : 'I like Jigglipeace',	author: 'Eitan Bolbol Gadol'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 		{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
+	// 	];
+	// });
 
 	
