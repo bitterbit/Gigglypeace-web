e6710bbc1d1be4fa33d8f05ea88f4ee6ce2d375c
eytanbar115 <eytanbar11@gmail.com>
2014-11-14 01:49:03 +0200

    changed router, addded mocks and get requests

--- router.js
+++ router.js
@@ -20,7 +20,10 @@
 
 		//$scope.discussions = [];
 		$scope.discussions = [
-			{title :'woho', firstComment : 'I like Jigglipeace',	author: 'Eitan Bolbol Gadol'},
+			{title :'woho', description : 'I like Jigglipeace',	publisher: 'Eitan Bolbol Gadol',
+			origin-lang:'he',number-of-views:50,
+			image-link:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMStbAt9oDDMHa2gXldmXHZUsagTS8YHavp09NxEP-jUIIku1gZQ'},
+			sideA :'side a',sideB:'side b'sideACount:50,sideBCount:40,neutralCount:30},
 			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
 			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
 			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
@@ -31,6 +34,12 @@
 			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
 			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
 		];
+		$http.get('/someurl').success
+		(function(data,status,headers,config) {
+			$scope.discussions=data;
+		}).error(function(data,status,headers,config) {
+
+		});
 	});
 
 	
