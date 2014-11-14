ac71c35fb10adc3d3c52deccdd934b04f4cfa4c7
galtashma <galtashma@gmail.com>
2014-11-14 03:27:07 +0200

    fixed worng commits to router

--- router.js
+++ router.js
@@ -1,45 +1,23 @@
-	// create the module and name it jigglyApp
-	var jigglyApp = angular.module('jigglyApp', ['ngRoute']);
+// create the module and name it jigglyApp
+var jigglyApp = angular.module('jigglyApp', ['ngRoute', 'textAngular']);
 
-	// configure our routes
-	jigglyApp.config(function($routeProvider) {
-		console.log($routeProvider);
-		$routeProvider
-			// route for the home page
-			.when('/', {
-				templateUrl : 'templates/home.html',
-				controller  : 'mainController'
-			})
-	});
+// configure our routes
+jigglyApp.config(function($routeProvider) {
 
-	// create the controller and inject Angular's $scope
-	jigglyApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		console.log('ASasAS');
-		console.log($scope);
-
-		//$scope.discussions = [];
-		$scope.discussions = [
-			{title :'woho', description : 'I like Jigglipeace',	publisher: 'Eitan Bolbol Gadol',
-			origin-lang:'he',number-of-views:50,
-			image-link:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMStbAt9oDDMHa2gXldmXHZUsagTS8YHavp09NxEP-jUIIku1gZQ'},
-			sideA :'side a',sideB:'side b'sideACount:50,sideBCount:40,neutralCount:30},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-			{title :'Tel Aviv', firstComment : 'Everybody hates it.',	author: 'Gal Bolbol Katan'},
-		];
-		$http.get('/someurl').success
-		(function(data,status,headers,config) {
-			$scope.discussions=data;
-		}).error(function(data,status,headers,config) {
-
-		});
-	});
-
+	console.log("in router");
 	
+	$routeProvider
+		// route for the home page
+		.when('/', {
+			templateUrl : 'templates/home.html',
+			controller  : 'homeController'
+		})
+		.when('/discussion', {
+			templateUrl : 'templates/discussion.html',
+			controller  : 'discussionController'
+		})
+		.when('/post', {
+			templateUrl : 'templates/post.html',
+			controller  : 'postController'
+		})
+});
