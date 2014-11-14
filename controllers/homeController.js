d5dcd7f3a5ffc8f2e2d8f686a8143808ab077372
eytanbar115 <eytanbar11@gmail.com>
2014-11-14 06:55:45 +0200

    bla bla bla last update

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -14,20 +14,15 @@
 	 console.log('this is the discussion controller');
 	});
 	// create the controller and inject Angular's $scope
-	angular.module('jigglyApp').controller('homeController', function($scope,$http) {
+	angular.module('jigglyApp').controller('homeController', function($scope) {
 	 // create a message to display in our view
 	
 	 $scope.discussions = [
 	{title :'woho', description : 'I like Jigglipeace',	publisher: 'Eitan Bolbol Gadol',
 	 			origin_lang:'he' ,number_of_views:50,
-			image_link:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ5zV9xuZjieKMGmEHa1t7rzWjhlx2penwXNA5Ka5GcvLCzznzI4A',
+			image_link:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMStbAt9oDDMHa2gXldmXHZUsagTS8YHavp09NxEP-jUIIku1gZQ',
 			sideA :'side a',sideB:'side b',sideACount:50,sideBCount:40,neutralCount:30},
 	 
 	 ];
-	 $http.get('/api/discussions?from=' +0 + '&to=' + 1).success(function (data, status, headers, config) {
-        console.log(data);
-    }).error(function (data, status, headers, config) {
-
-    });
 	});
 	
