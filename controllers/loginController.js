83b5c05f247435cfa3f0fd5e317f3c980d31af74
matanw <matanwis@gmail.com>
2014-11-14 04:26:40 +0200

    add login (start)

---
+++ controllers/loginController.js
@@ -0,0 +1,5 @@
+﻿angular.module('jigglyApp').controller('loginController', function ($scope) {
+    console.log('this is the login controller');
+    $scope.username = "uu";
+    $scope.password = "pp";
+});
